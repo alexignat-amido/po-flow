39ba4772f9067b07016330ecb0132e2755c4b953
Archie Brand-Williamson <archie.brand@amido.com>
2019-01-15 10:29:28 +0000

    Added log bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,9 +12,21 @@ provider "aws" {
   version = "1.49.0"
 }
 
-resource "aws_s3_bucket" "default" {
-  bucket = "po-flow"
+resource "aws_s3_bucket" "logs" {
+
+  bucket = "${var.site_name}-site-logs"
+  acl = "log-delivery-write"
+}
+
+resource "aws_s3_bucket" "www_site" {
+
+  bucket = "www.${var.site_name}"
   acl    = "public-read"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.bucket}"
+    target_prefix = "www.${var.site_name}/"
+  }
 
   website {
     index_document = "index.html"
